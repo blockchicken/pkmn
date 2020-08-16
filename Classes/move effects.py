a9422cf758c35abaf7dcc6f1d9b172ac669052e9
Arie Halpern <arie@ariehalpern.com>
2020-08-16 00:14:18 -0700

    Reconfiging moves, have to add new function for each move, fun!

---
+++ Classes/move effects.py
@@ -0,0 +1,32 @@
+def UseHelpingHand(User, Player):
+    pass
+
+def UseSwordsDance(User):
+    pass
+
+def UseDragonPulse(User, Target):
+    pass
+
+def UseTailwind(User, TargetPlayer):
+    pass
+
+def UseTrickRoom(User):
+    pass
+
+def UseStickyWeb(User, TargetPlayer):
+    pass
+
+def UseSurf(User, Player, TargetPlayer):
+    pass
+
+def UseDazzlingGleam(User, TargetPlayer):
+    pass
+
+def UseOutrage(User, TargetPlayer):
+    pass
+
+def UseSandstorm(User):
+    pass
+
+def UseProtect(User):
+    pass
