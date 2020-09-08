c57e16a235275fa7c96939f29262df440f160c93
Arie Halpern <arie@ariehalpern.com>
2020-09-07 22:04:04 -0700

    what am i doing with my life anymore

---
+++ Code/battle init.py
@@ -0,0 +1,12 @@
+### Initialize the players, teams, etc.
+
+# Import Monsters
+
+# Import Moves
+
+# Import Teams 
+### Note, this should only take the two teams being played. 
+
+# Player Init
+
+# 
